959a2a3237ce30bb692f817a464c10b98d59925a
Dustin Pfannenstiel <dustin@nth-estate.com>
2016-02-11 15:39:07 -0600

    Hold on to your butts.

---
+++ Probable Chainsaw/SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Dustin Pfannenstiel on 2/11/16.
+//  Copyright © 2016 Dustin Pfannenstiel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
